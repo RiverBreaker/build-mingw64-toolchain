0de54ce24e9b2ba1ed2fda0f8ee68ee54e6cad45
RiverBreaker <lyq203q@163.com>
2025-08-13 13:24:51 +0800

    11

---
+++ scripts/download_dep.sh
@@ -0,0 +1,23 @@
+sudo apt-get update -qq
+sudo apt-get install -y \
+    build-essential \
+    git \
+    g++ g++-multilib \
+    gcc gcc-multilib \
+    gperf \
+    bison \
+    flex \
+    texinfo \
+    help2man \
+    gawk \
+    libtool \
+    automake \
+    autoconf \
+    pkg-config \
+    zlib1g-dev \
+    libz-mingw-w64-dev \
+    libiconv-dev \
+    libgettextpo-dev \
+    gettext \
+    mingw-w64-tools \
+    zip unzip
