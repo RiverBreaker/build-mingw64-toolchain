7ff535c6b7d0eeed3819428d5e0fdddcc647c7b7
云梦泽 <148955759+RiverBreaker@users.noreply.github.com>
2025-08-13 13:16:01 +0800

    Create build_mingw_binutils.sh

---
+++ mingw64_scripts/build_mingw_binutils.sh
@@ -0,0 +1,63 @@
+export PREFIX="${{ github.workspace }}/build/mingw64"
+export BUILD_TEMP="${{ github.workspace }}/build/build-temp"
+export SRC_DIR="${{ github.workspace }}/build/src"
+export OUTPUT_DIR="${{ github.workspace }}/build/ubuntu-tools/mingw64"
+export TARGET=x86_64-w64-mingw32
+export BUILD=x86_64-pc-linux-gnu
+export HOST=x86_64-w64-mingw32
+export PATH=$PATH:$OUTPUT_DIR/bin
+
+cd $BUILD_TEMP
+for d in build-mingw-binutils build-mingw-m4 build-mingw-libtool; do
+    [ -d "$d" ] && rm -rf "$d"
+done
+mkdir -p build-mingw-binutils build-mingw-m4 build-mingw-libtool
+echo "mkdir build-mingw-binutils build-mingw-m4 build-mingw-libtools"
+
+binutils_src=$(realpath --relative-to="${BUILD_TEMP}/build-mingw-binutils" "${SRC_DIR}/binutils")
+m4_src=$(realpath --relative-to="${BUILD_TEMP}/build-mingw-m4" "${SRC_DIR}/m4")
+libtool_src=$(realpath --relative-to="${BUILD_TEMP}/build-mingw-libtool" "${SRC_DIR}/libtool")
+
+cd $BUILD_TEMP/build-mingw-binutils
+${binutils_src}/configure \
+    --target=$TARGET \
+    --build=$BUILD \
+    --prefix=$PREFIX \
+    --host=$HOST \
+    --target=$TARGET \
+    --enable-shared \
+    --disable-nls \
+    --enable-ld \
+    --disable-lto \
+    --with-mpc=$PREFIX \
+    --with-gmp=$PREFIX \
+    --with-mpfr=$PREFIX \
+    --with-cloog=$PREFIX \
+    --with-isl=$PREFIX
+echo "Configure Binutils completed."
+make -j1 && make install
+echo "Build Binutils completed."
+
+cd $BUILD_TEMP/build-mingw-m4
+${m4_src}/configure \
+    --build=$BUILD \
+    --prefix=$PREFIX \
+    --host=$HOST \
+    --enable-threads=windows \
+    --disable-c++
+echo "Configure M4 completed."
+make -j1 && make install
+echo "Build M4 completed."
+
+cd $BUILD_TEMP/build-mingw-libtool
+${libtool_src}/configure \
+    --build=$BUILD \
+    --prefix=$PREFIX \
+    --host=$HOST \
+    --with-gnu-ld \
+    --enable-ltdl-install \
+    --enable-shared \
+    --disable-static
+echo "Configure Libtool completed."
+make -j1 && make install
+echo "Build Libtool completed."
